b62b9ff2c9527497603d865ae9010a880d972b36
kun <tjdrns27@naver.com>
2021-12-28 22:22:13 +0900

    A

--- protocol/Nanopore/LSK109/lsk109_1_endprep_flongle.py
+++ protocol/Nanopore/LSK109/lsk109_1_endprep_flongle.py
@@ -13,19 +13,20 @@ metadata = {
 
 protocol = simulate.get_protocol_api("2.11")
 
-# Module setting.
+# ================ Module setting.
 # for the movement of Pippete Input all of modules in Device!
 
 module_magnetic = protocol.load_module('magnetic module gen2', '4')
 module_thermocycler = protocol.load_module("Thermocycler Module")
 
-# Deck setting
+# ================ Deck setting
 ## Labware setting
 enzyme_rack = protocol.load_labware("opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap", 6)
 falcon_rack = protocol.load_labware("opentrons_15_tuberack_falcon_15ml_conical", 1)
 final_plate = protocol.load_labware("nest_96_wellplate_100ul_pcr_full_skirt", 5)
 thermo_plate = protocol.load_labware("nest_96_wellplate_100ul_pcr_full_skirt", 12)
 mag_plate = module_magnetic.load_labware("nest_96_wellplate_100ul_pcr_full_skirt", 4)
+trash = protocol.loaded_labwares[12]["A1"]
 
 tiprack_20_sin = protocol.load_labware("opentrons_96_tiprack_20ul", 2)
 tiprack_300_sin = protocol.load_labware("opentrons_96_tiprack_300ul", 3)
@@ -40,6 +41,8 @@ repair_enzyme = enzyme_rack["A1"]
 repair_buf = enzyme_rack["A2"]
 endprep_enzyme = enzyme_rack["A3"]
 endprep_buf = enzyme_rack["A4"]
+water = enzyme_rack["A5"]
+
 
 ## Sample Wells
 start_position = 0 ## start from A1
@@ -57,7 +60,7 @@ tmp_well = [final_plate.wells()[i] for i in range(start_position, start_position
 final_well = [final_plate.wells()[i] for i in range(start_position, start_position + sample_number)]
 mag_well = [mag_plate.wells()[i] for i in range(start_position, start_position + sample_number)]
 
-# PROTOCOL STEPS
+# ================ PROTOCOL STEPS
 ## Enzyme reaction
 p20_sin.transfer(1, repair_enzyme, dna_well, rate=0.5, new_tip="always")
 p20_sin.transfer(1, repair_buf, dna_well, rate=0.5, new_tip="always")
@@ -83,65 +86,66 @@ p300_sin.mix(5, 300, ampure_beads.bottom(z=3), rate=0.5)
 p300_sin.touch_tip()
 p300_sin.drop_tip()
 
-p300_sin.transfer(45, ampure_beads.top(z=-2), dna_well, touch_tip=True, mix_after=(3, 30), rate=0.5, new_tip="always")
+p300_sin.transfer(45, ampure_beads.top(z=3), dna_well, touch_tip=True, mix_after=(3, 30), rate=0.5, new_tip="always")
 
-## transfer to tube for Hula Mixer
-p300_sin.transfer(60, dna_well, final_well)
+## transfer to tube for Hula Mixer (5)
+p300_sin.transfer(75, dna_well, final_well, rate=0.5, touch_tip=True)
+protocol.pause("for Hula Mixer")
 
-# Pause for Hula Mixer/Spin Down (8-9)
-ctx.pause('''Incubate on Hula Mixer for 5 minutes and spin down the samples.
-                Then place back on the magnet and click resume.''')
+## Mixed tube to magnetic plate (6)
+p300_sin.transfer(75, tmp_well, mag_well, rate=0.5, touch_tip=True, new_tip="always")
 
-# Engage Magnet and Delay for 5 Minutes (10)
-magnet(5)
-# Remove Supernatant (11)
-for well, side in zip(mag_plate_wells, sides):
-    pick_up(m300)
-    remove_supernatant(100, well, trash, side)
-    m300.drop_tip()
+## Engage Magnet and Delay for 5 Minutes (6)
+module_magnetic.engage(height_from_base=5)
+protocol.delay(minutes=5)
 
-# (12-14)
+## Remove Supernant (7)
+def remove_supernantant(pipette, volume, src, dest):
+    pipette.pick_up_tip()
+    pipette.flow_rate.aspirate=20
+    pipette.aspirate(volume, src.bottom().move(types.Point(x=0, y=0, z=0)))
+    pipette.dispense(volume, dest)
+    pipette.blow_out(trash=True)
+    pipette.drop_tip()
+
+## First supernant
+for src in mag_well:
+    remove_supernantant(p300_sin, 50, src)
+
+## Wash Beads with Ethanol (12)
 for _ in range(2):
-    # Wash Beads with Ethanol (12)
-    for well in mag_plate_wells:
-        pick_up(m300)
-        m300.aspirate(200, ethanolTrack.tracker(200))
-        m300.dispense(200, well.top(-3))
-        m300.mix(3, 100)
-        m300.drop_tip()
+    ## Ethanol
+    p300_sin.pick_up_tip()
+    for src in mag_well:
+        p300_sin.aspirate(190, ethanol_70, rate=0.5)
+        p300_sin.dispense(190, src.top(-3), rate=0.5)
+    p300_sin.drop_tip()
+    ## Remove
+    for src in mag_well:
+        remove_supernantant(p300_sin, 200, src)
 
-    # Remove Supernatant (11)
-    for well, side in zip(mag_plate_wells, sides):
-        pick_up(m300)
-        remove_supernatant(100, well, trash, side)
-        m300.drop_tip()
+## Pause and Remove Samples for Spin Down (15)
+module_magnetic.disengage()
+protocol.pause('''Spin down and place samples back on the maget. Then click resume.''')
 
-# Pause and Remove Samples for Spin Down (15)
-mag_mod.disengage()
-ctx.pause('''Spin down and place samples back on the maget.
-            Then click resume.''')
-magnet(1)
+## Remove Residual Ethanol (16)
+module_magnetic.engage(height_from_base=5)
+protocol.delay(minutes=1)
+for src in mag_well:
+    remove_supernantant(p20_sin, 15, src)
+module_magnetic.disengage()
+protocol.delay(seconds=30)
 
-# Remove Residual Ethanol (16)
-for well, side in zip(mag_plate_wells, sides):
-    pick_up(m300)
-    remove_supernatant(100, well, trash, side)
-    m300.drop_tip()
+## Add 61 uL of Nuclease-free Water (17)
+for src in mag_well:
+    p300_sin.transfer(50, water, src.top(-3), mix_after=(3,60), rate=0.5, new_tip="always")
 
-# Add 61 uL of Nuclease-free Water (17)
-mag_mod.disengage()
-for well in mag_plate_wells:
-    m300.transfer(61, nfa, well.top(-3), mix_after=(3, 60))
-ctx.delay(minutes=2, msg='Incubating at Room Temperature for 2 minutes...')
+module_magnetic.engage()
+protocol.delay(minutes=5)
 
-# Engage Magnet (18)
-magnet(5)
+## Elution
+for src, dest in zip(mag_well, final_well):
+    p20_sin.pick_up_tip()
+    p20_sin.
 
-# Transfer Eluate into MIDI plate for part 2 (19)
-for well, dest, side in zip(mag_plate_wells, final_plate_wells, sides):
-    pick_up(m300)
-    remove_supernatant(100, well, dest, side)
-    m300.drop_tip()
-
-temp_mod.deactivate()
-mag_mod.disengage()
+module_magnetic.disengage()
