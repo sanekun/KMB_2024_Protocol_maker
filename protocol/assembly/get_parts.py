bb571ee813bd786b3a42ffc25ed005bde92b7290
Lelp27 <Lelp27@github.com>
2022-03-15 19:07:38 +0900

    lab

--- protocol/assembly/get_parts.py
+++ protocol/assembly/get_parts.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import logging
 
 """
 1. Part 정보들 불러오기 (Excel)
@@ -62,39 +63,113 @@ for i in A:
 은 실험노트에도 바로 사용할 수 있도록 사람이 읽을 수 있게하자.
 """
 # MW 까지는 받아오지만 Volume은 정한 Parameter에 맞춰서 변경 할 수 있도록 해야한다.
-
-# Parameters
-
-pro = ["P1", "P2", 'P3', 'P4']
-rbs = ["R1", "R2", 'R3']
-ter = ["T1", "T2"]
+# 우선 Library  고려 없이 진행하도록 ? 단일 파트만..
 
 
-# MW (fmol)
-cds_MW = 112
-part_MW = 56
+# Functions
 
 class dna:
-    def __init__(self, length, conc, MW, Name = None):
+    # Basic information
+    ## MW can calculate with python or Excel.
+    def __init__(self, length, conc, MW, well, name):
         self.length = length
         self.conc = conc
         self.MW = MW
-        self.Name = Name
+        self.name = name
+        self.well = well
+
+    # get optimal volume based on [part, cds, vector]_MW parameter.
+    def get_volume(self, goal_MW):
+        final = (goal_MW/self.MW).round(2)
+        return (final)
+
+def db_check(part_list, db):
+    for i in part_list:
+        if i in db["No"].values:
+            pass
+        else:
+            logging.critical("{} is not in DB !".format(i))
+            return True
+
+def assembly(pro=[], rbs=[], ter=[], cds=[]):
+
+    # Check every part is in DB with Part's No
+    for n1, n2 in zip([pro, rbs, ter, cds], [pro_db, rbs_db, ter_db, cds_db]):
+        if db_check(n1, n2):
+            return ("Protocol Break")
+        else:
+            pass
+
+    return ("Every Part is in DB print Assembly Well")
+
+
+
+#def main():
 
 db_path = "/mnt/kun/box/-20/Part_DB.xlsx"
 
 pro_db = pd.read_excel(db_path, sheet_name="pro")
 rbs_db = pd.read_excel(db_path, sheet_name="rbs")
 ter_db = pd.read_excel(db_path, sheet_name="ter")
-# cds는 DB에 없을 경우가 많을 것 같으니 Unknown을 쓸 수 잇도록 (모든 파트가)
 cds_db = pd.read_excel(db_path, sheet_name="cds")
 
+# Parameters
+## input Part's No in DB
+pro = ["p1"]
+rbs = ["r1"]
+ter = ["t1"]
+cds = ["c1"]
 
-"P1" in pro_db["No"]
+# MW (fmol)
+part_MW = 112
+cds_MW = 112
+vector_MW = 56
+
+assembly(pro, rbs, ter)
+
+pro_dna = []
+for i in pro:
+    tmp = pro_db[pro_db["No"] == i]
+    pro1 = dna(length = tmp.full_length.values,
+            conc = tmp.concentration.values,
+            MW = tmp.MW.values,
+            name = tmp.Name.values,
+            well = tmp.Well.values)
+
+for i in rbs:
+    tmp = rbs_db[rbs_db["No"] == i]
+    rbs1 = dna(length = tmp.full_length.values,
+            conc = tmp.concentration.values,
+            MW = tmp.MW.values,
+            name = tmp.Name.values,
+            well = tmp.Well.values)
+
+for i in ter:
+    tmp = ter_db[ter_db["No"] == i]
+    ter1 = dna(length = tmp.full_length.values,
+            conc = tmp.concentration.values,
+            MW = tmp.MW.values,
+            name = tmp.Name.values,
+            well = tmp.Well.values)
+
+for i in cds:
+    tmp = cds_db[cds_db["No"] == i]
+    cds1 = dna(length = tmp.full_length.values,
+            conc = tmp.concentration.values,
+            MW = tmp.MW.values,
+            name = tmp.Name.values,
+            well = tmp.Well.values)
 
 
-#assembly(pro = [], rbs = [], ter = [], cds = [])
+rbs1.get_volume(112)
+pro1.get_volume(112)
+ter1.get_volume(112)
+cds1.get_volume(56)
+ter1.MW
 
 
-B = dna(length = 50, concentration = 100, MW = 200)
 
+# Output Simulate
+Well1 = assembly(pro, rbs, ter, cds, parameters = [])
+
+# 우선 SIngle Part로 진행하고 이후 고민해 보도록 하자.
