99d5862fff3219cc5874ebd46ad8d78a197c0835
Lelp27 <Lelp27@github.com>
2022-05-12 18:14:06 +0900

    taggingPCR

--- functions.py
+++ functions.py
@@ -7,11 +7,21 @@ metadata = {
     'description': 'Basic OT2 Functions'
 }
 
-protocol = simulate.get_protocol_api('2.11')
+protocol=simulate.get_protocol_api('2.11')
 
-def enzyme_transfer(pipette, volume, src, dest, delay_second=[0, 0],
-                  top_delay=False, asp_rate=None, dis_rate=None,
-                  mix_after=False, drop_tip = True):
+def enzyme_transfer(pipette, volume, src, dest, delay_second=None,
+                top_delay=False, asp_rate=None, dis_rate=None,
+                mix_after=False, drop_tip = True):
+
+    """
+    basic transfer method for Liquid.
+    can adjust pipette delay, speed, mix ...
+
+    Parameters
+    ----------------------------------------
+
+    """
+    
     if asp_rate:
         pipette.flow_rate.aspirate=asp_rate
     if dis_rate:
@@ -34,7 +44,6 @@ def enzyme_transfer(pipette, volume, src, dest, delay_second=[0, 0],
         except:
             pass
         pipette.mix(mix_after[0], mix_after[1])
-    pipette.blow_out()
     if top_delay:
         pipette.move_to(src.top(z=-3))
         protocol.delay(seconds=top_delay[1])
@@ -42,8 +51,21 @@ def enzyme_transfer(pipette, volume, src, dest, delay_second=[0, 0],
         pipette.drop_tip()
 
 
-class Reagent():
-    def __init__(self, name, wells, cols, vol_per_well):
-        self.name
-        self.wells
-        self.cols
+def get_well(well):
+    #check가 되도록해야할듯.
+    if type(well) == list:
+        return (well[0])
+    else:
+        return (well)
+
+
+def well_mix(protocol, pipette, dest, mix_params, flow_rate):
+    if pipette._has_tip != True:
+        pipette.pick_up_tip()
+    pipette.flow_rate = flow_rate
+    pipette.move_to(dest.bottom())
+    pipette.mix(mix_params[0], mix_params[1])
+    pipette.blow_out()
+    pipette.move_to(dest.top(z=-3))
+    protocol.delay(seconds=2)
+    pipette.drop_tip()
--- protocol/tagging_PCR_plate/96well_pooling.py
+++ protocol/tagging_PCR_plate/96well_pooling.py
@@ -1,56 +1,141 @@
-from opentrons import protocol_api
-import math
+from opentrons import types, protocol_api, simulate
+
+# 96 well Pooling
+# Not Verified
 
 metadata = {
-    'protocolName': '96 well plate pooling',
+    'protocolName': 'Tagging colony PCR (96 well plate based)',
     'author': 'Seong-Kun Bak <tjdrns27@kribb.re.kr>',
-    'apiLevel': '2.11'   # CHECK IF YOUR API LEVEL HERE IS UP TO DATE
+    'apiLevel': '2.11',
+    'description': 'Using forward [1-12] & Reverse [1-8] Primer Make 96 sperated tag combination in 96well plate'
 }
+
+"""
+Add three component.
+1) Set Parameters [num_of_plate, target_columns]
+"""
+
+protocol=simulate.get_protocol_api('2.11')
 
 def run(protocol: protocol_api.ProtocolContext):
 
-    # Deck setting
+    # parmeters
+    num_of_plate = 2
+    target_columns = 12
 
-        # Module setting.
-        # for the movement of Pippete Input all of modules in Device!
-
-    # module_magnetic = protocol.load_module('magnetic module gen2', '4')
+    # Functions
     
-        # Plate setting
-    source_plate = protocol.load_labware("bioneer_96_tuberack_200ul", 6)
-    dest_plate = protocol.load_labware("bioneer_96_rack_eppendorf_plate_200ul", 5)
+    def enzyme_transfer(pipette, volume, src, dest, delay_second=None,
+                    top_delay=False, asp_rate=None, dis_rate=None,
+                    mix_after=False, drop_tip = True):
+        # top_delay = list, delay_second= list, mix_after = list
+        
+        if asp_rate:
+            pipette.flow_rate.aspirate=asp_rate
+        if dis_rate:
+            pipette.flow_rate.dispense=dis_rate            
+        
+        if pipette._has_tip == False:
+            pipette.pick_up_tip()
 
-        # tiprack
-    tiprack_20_sin = protocol.load_labware("opentrons_96_tiprack_20ul", 3)
-    tiprack_20_mul = protocol.load_labware("opentrons_96_tiprack_20ul", 2)
+        pipette.aspirate(volume, src)
+        protocol.delay(seconds=delay_second[0])
+        if top_delay:
+            pipette.move_to(src.top(z=-3))
+            protocol.delay(seconds=top_delay[0])
+        pipette.dispense(volume, dest)
+        protocol.delay(seconds=delay_second[1])
+        if type(mix_after) == list:
+            try:
+                pipette.flow_rate.aspirate=mix_after[2]
+                pipette.flow_rate.dispense=mix_after[2]
+            except:
+                pass
+            pipette.mix(mix_after[0], mix_after[1])
+        if top_delay:
+            pipette.move_to(src.top(z=-3))
+            protocol.delay(seconds=top_delay[1])
+        if drop_tip:    
+            pipette.drop_tip()
 
-        # Pipette 
-    p20_sin = protocol.load_instrument("p20_single_gen2", "right", tip_racks=[tiprack_20_sin])
-    p20_mul = protocol.load_instrument("p20_multi_gen2", "left", tip_racks=[tiprack_20_mul])
+    def get_well(well):
+        if type(well) == list:
+            return (well[0])
+        else:
+            return (well)
 
-        # Tip rack start positions
-    p20_sin.starting_tip = tiprack_20_sin.well("A1")
-    p20_mul.starting_tip = tiprack_20_mul.well("A1")
-    dest_well = dest_plate["A1"]
+    def well_mix(pipette, dest, mix_params, flow_rate):
+        if pipette._has_tip != True:
+            pipette.pick_up_tip()
+        pipette.flow_rate = flow_rate
+        pipette.move_to(dest.bottom())
+        pipette.mix(mix_params[0], mix_params[1])
+        pipette.blow_out()
+        pipette.move_to(dest.top(z=-3))
+        pipette.delay(seconds=2)
+        pipette.drop_tip()
 
-        # Liquids name and positions
-        # it should set appropriate position
+    # Deck Setting
+    ## Modules  
+    module_thermocycler = protocol.load_module("thermocycler Module")
 
-    pcr_product = source_plate.rows_by_name()["A"][0:8] # start to end position of source well
-    tmp_pooling = source_plate.columns_by_name()["11"]
+    ## Racks
+    pooled_plate = protocol.load_labware('biorad_96_wellplate_200ul_pcr', 9) 
+    trash = protocol.loaded_labwares[12]["A1"]
+    for n in range(num_of_plate):
+        globals()[f'source_plate{n+1}'] = protocol.load_labware('biorad_96_wellplate_200ul_pcr', n+1)
+
+    tiprack_20_1 = protocol.load_labware("opentrons_96_tiprack_20ul", 6)
+    tiprack_20_2 = protocol.load_labware("opentrons_96_tiprack_20ul", 5)
+
+    tipracks_20 = [tiprack_20_1, tiprack_20_2]
+
+    p20_sin = protocol.load_instrument("p20_single_gen2", "right", tip_racks=tipracks_20)
+    p20_mul = protocol.load_instrument("p20_multi_gen2", "left", tip_racks=tipracks_20)
+
+    ## Start Tiprack positions
+    p20_sin.starting_tip = tiprack_20_1.well("A1")
+    p20_mul.starting_tip = tiprack_20_1.well("A1")
+
+    ## Well Positions
+    tmp_well, final_well = [], []
+    for n in range(num_of_plate):
+        tmp_well.append(pooled_plate.columns()[-(n+1)])
+        final_well.append(pooled_plate.columns()[0][(n)])
     
-        # tmp_pooling
-    p20_mul.pick_up_tip()
-    p20_mul.consolidate(5, pcr_product, tmp_pooling[0], rate=0.5, new_tip="never")
-    p20_mul.touch_tip()
-    p20_mul.mix(3, 10, tmp_pooling[0], rate=0.5)
-    p20_mul.touch_tip()
-    p20_mul.drop_tip()
+    ## Protocol RUN
 
-        # final_pooling
-    p20_sin.pick_up_tip()
-    p20_sin.consolidate(5, tmp_pooling, dest_well, rate=0.5, new_tip="never")
-    p20_sin.touch_tip()
-    p20_sin.mix(2, 10, dest_well, rate=0.5)
-    p20_sin.touch_tip()
-    p20_sin.drop_tip()
+    ### First 8 - channel Pooling
+    for n in range(num_of_plate):
+        src_col = eval(f'source_plate{n+1}').rows_by_name()['A'][0:target_columns]
+        dest = get_well(tmp_well[n])
+        vol = 10
+        tip_trial = 0
+        
+        for src in src_col:
+            enzyme_transfer(p20_mul, vol, src, dest,
+                            asp_rate=10, dis_rate=10, drop_tip=False)
+            tip_trial += 1
+
+            if tip_trial == 4:
+                p20_mul.drop_tip()
+        
+        ### Mix
+        well_mix(p20_mul, dest=dest, mix_params=[5, 10], flow_rate=10)
+
+    ### Single Chanel Pooling
+    for n in range(num_of_plate):
+        src_col = tmp_well[n]
+        dest = final_well[0]
+        vol = 10
+        tip_trial = 0
+
+        for src in src_col:
+            enzyme_transfer(p20_sin, vol, src, dest,
+                            asp_rate=10, dis_rate=10, drop_tip=False)
+            tip_trial += 1
+            
+            if tip_trial == 4:
+                p20_sin.drop_tip()
+
+        well_mix(p20_sin, dest=dest, mix_params=[5, 10], flow_rate=10)
