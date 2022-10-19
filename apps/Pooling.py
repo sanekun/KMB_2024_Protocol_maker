3570bd414ffe480d02051c2d54fa55abc24d4493
kun_laptop <tjdrns227@gmail.com>
2022-10-19 23:27:41 +0900

    pooling

--- apps/Pooling.py
+++ apps/Pooling.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import string
+
+# by column
+# start col, end col, interval
+# Duplication Number
 
 def app():
     # DIV
@@ -6,3 +11,19 @@ def app():
     st.caption("Author : Seong-Kun Bak <<tjdrns227@gmail.com>>")
     st.markdown("---")
     st.text("")
+
+    col1, col2 = st.columns([2, 1])
+
+    alphabet = list(string.ascii_uppercase)[0:8]
+    with col1:
+        st.selectbox('Start Column', options=alphabet)
+        st.selectbox('End Column', options=[i for i in alphabet[::-1]])
+        st.number_input('Interval', min_value=1, max_value=6)
+
+    with col2:
+        st.selectbox('Input Position', options = [1,2,3], help='The Plate Position want to pool')
+        st.selectbox('Output Position', options = [2,3], help='The Pooled Plate position')
+    st.download_button('Download Protocol', data="A")
+
+if __name__ == '__main__':
+    app()
