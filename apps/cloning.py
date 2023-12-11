3cd194f1814a65da5e15f48587e5bf5ebad42ed4
Lelp27 <sanekun@kaist.ac.kr>
2023-12-11 18:01:44 +0900

    Cloning

--- apps/cloning.py
+++ apps/cloning.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 # Statics
-def plate_df():
+def empty_plate_df():
     # Define rows and columns
     rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
     columns = range(1, 13)
@@ -12,98 +12,175 @@ def plate_df():
     plate_df = pd.DataFrame(index=rows, columns=columns)
     return plate_df
 
-def plate_initialization(i):
-    if f"plate_{i}_df" not in st.session_state:
-        st.session_state[f"plate_{i}_df"] = plate_df()
-    if f"plate_{i}_name" not in st.session_state:
-        st.session_state[f"plate_{i}_name"] = ""
+def plate_initialization(i, plate_type: str, table_type='empty'):
+    initial_tables = {
+        'empty': empty_plate_df(),
+        'PCR': pd.DataFrame({
+            'Well': ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3', 'A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4', 'A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5', 'A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6', 'A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7', 'A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8', 'A9', 'B9', 'C9', 'D9', 'E9', 'F9', 'G9', 'H9', 'A10', 'B10', 'C10', 'D10', 'E10', 'F10', 'G10', 'H10', 'A11', 'B11', 'C11', 'D11', 'E11', 'F11', 'G11', 'H11', 'A12', 'B12', 'C12', 'D12', 'E12', 'F12', 'G12', 'H12'],
+            'Name': [None for _ in range(96)],
+            'DNA1': [None for _ in range(96)],
+            'DNA2': [None for _ in range(96)],
+            'DNA3': [None for _ in range(96)],
+            'Enzyme (12.5)': ["KODone" for _ in range(96)],
+            'DW (25)': ['DW' for _ in range(96)]
+        }),
+        'Assembly': pd.DataFrame({
+            'Well': ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3', 'A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4', 'A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5', 'A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6', 'A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7', 'A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8', 'A9', 'B9', 'C9', 'D9', 'E9', 'F9', 'G9', 'H9', 'A10', 'B10', 'C10', 'D10', 'E10', 'F10', 'G10', 'H10', 'A11', 'B11', 'C11', 'D11', 'E11', 'F11', 'G11', 'H11', 'A12', 'B12', 'C12', 'D12', 'E12', 'F12', 'G12', 'H12'],
+            'Name': [None for _ in range(96)],
+            'DNA1': [None for _ in range(96)],
+            'DNA2': [None for _ in range(96)],
+            'Enzyme (10)': ['NEBuilder HiFi Master Mix'] * 96,
+            'DW (20)': ['DW'] * 96
+        }),
+    }
+    if f"{plate_type}_plate_{i}_name" not in st.session_state:
+        st.session_state[f"{plate_type}_plate_{i}_name"] = ""
+    if f"{plate_type}_plate_{i}_df" not in st.session_state:
+        st.session_state[f"{plate_type}_plate_{i}_df"] = initial_tables[table_type]
 
-def plate_transformation(df, type):
-    assert type in ["wide", "long"], "Type Error"
-    if type == 'long':
+def plate_transformation(df, data_form):
+    assert data_form in ["wide", "long"], "data_form Error"
+    if data_form == 'long':
         new_df = (df
         .reset_index()
         .melt(id_vars=['index'], value_name='Value')
         .rename(columns={'index':'Row', 'variable':'Column'})
         )
+        new_df['well'] = new_df['Row'] + new_df['Column'].astype(str)
+        new_df = new_df.set_index('well')['Value'].to_frame()
+        
         return new_df
     
-    elif type == 'wide':
-        new_df = df.pivot(index='Row', columns='Column', values='Value')
+    elif data_form == 'wide':
+        new_df = df.reset_index()
+        new_df[['Row', 'Column']] = new_df['well'].str.extract(r'([A-Z]+)(\d+)')
+        new_df['Column'] = new_df['Column'].astype(int)
+        new_df = new_df[['Row', 'Column', 'Value']]
+        
+        new_df = new_df.pivot(index='Row', columns='Column', values='Value').sort_index(axis=1)
         new_df.columns.name = None
         new_df.index.name = None
+        
         return new_df
 
-if 'tf_df' not in st.session_state:
-    st.session_state['tf_df'] = plate_df()
+def Example():
+    PCR_df = pd.DataFrame({
+        'Well': ['A1', 'B1', 'C1', 'D1', 'F1'],
+        'Name': ['S1', 'S2', 'S3', 'S4', 'S5'],
+        'DNA1': ['pACBB_4-5', 'pACBB_4-5', 'pACBB_4-5', 'pACBB_4-5', 'pACBB_4-5'],
+        'DNA2': ['pACBB_vec-F1', 'pACBB_vec-F2', 'pACBB_T7-F1', 'pACBB_T7-F2', 'pET28_MCS-F1'],
+        'DNA3': ['pACBB_vec-R1', 'pACBB_vec-R2', 'pACBB_T7-R1', 'pACBB_T7-R2', 'pET28_MCS-R1'],
+        'Enzyme (12.5)': ['KODone', 'KODone', 'KODone', 'KODone', 'KODone'],
+        'DW (25)': ['DW', 'DW', 'DW', 'DW', 'DW']
+        }),
+    Assembly_df = pd.DataFrame({
+        'Well': ['A9', 'B9', 'C9', 'D9', 'E9'],
+        'Name': ['vec1', 'vec2', 'vec3', 'vec4', 'vec5'],
+        'DNA1': ['S1', 'S1', 'S2', 'S1', 'S3'],
+        'DNA2': ['S2', 'S3', 'S3', 'S2', None],
+        'DNA3': [None, None, None, 'S3', None],
+        'Enzyme (10)': ['NEBuilder HiFi Master Mix'] * 5,
+        'DW (20)': ['DW'] * 5
+        }),
 
-
+st.session_state['labwares'] = ["nest_96_wellplate_200ul_flat"]
 st.set_page_config(page_title="Cloning",
                    layout="wide")
 
 # Main
-num_of_plate = st.number_input("Number of Plate", min_value=1, step=1, value=2,
-                               help="Plate 등록 (Transformation 까지)")
-reaction_plate = st.selectbox("Reaction Plate", options=[f"Plate_{i+1}" for i in range(num_of_plate)],
-             help="Reaction Plate should be on Thermocycler")
 st.button('Use Example')
 
-with st.expander("Input Plate", expanded=True):
-    plates = st.tabs([f"Plate_{i+1}" for i in range(num_of_plate)])
+## DNA Plate
+with st.expander("DNA Plate", expanded=True):
+    plate_type = 'DNA'
+    number_of_plate = st.number_input(f"Number of {plate_type}_plate", min_value=1, step=1, value=2, key=f'num_of_{plate_type}_plate')
+    plates = st.tabs([f'{plate_type}_Plate_{i+1}' for i in range(number_of_plate)])
 
-for i in range(len(plates)):
-    plate_initialization(i)
-    with plates[i]:
-        st.toggle("Long Form", key=f"plate_{i}_toggle")
-        if not st.session_state[f'plate_{i}_toggle']:
-            st.session_state[f"plate_{i}_df"] = st.data_editor(data=st.session_state[f"plate_{i}_df"],
-                                                            use_container_width=True,
-                                                            key=f'plate_{i}_wide')
-        else:
-            st.session_state[f'plate_{i}_df'] = plate_transformation(df = st.data_editor(plate_transformation(st.session_state[f'plate_{i}_df'], type='long'),
-                                                                                        use_container_width=True, key=f'plate_{i}_long'),
-                                                                    type = 'wide')
-        st.markdown("---")
-        
-        st.text_input("Name", value=st.session_state[f"plate_{i}_name"], key=f"plate_{i}_name")
-        st.selectbox("Type", options=["nest_96_wellplate_200ul_flat"], key=f"plate_{i}_type")
-        st.file_uploader("Upload", key=f"plate_{i}_uploader", disabled=True)
+    for i in range(len(plates)):
+        plate_initialization(i, plate_type=plate_type, table_type='empty')
+        with plates[i]:
+            st.text_input("Name", value=st.session_state[f'{plate_type}_plate_{i}_name'], key=f'{plate_type}_plate_{i}_name')
+            st.selectbox("Labware", options=st.session_state['labwares'], key=f'{plate_type}_plate_{i}_labware')
 
-with st.expander("PCR", expanded=True):
-    st.data_editor(
-        pd.DataFrame({
-        'Well': ['A1', 'B1', 'C1', 'D1', 'F1'],
-        'Name': ['S1', 'S2', 'S3', 'S4', 'S5'],
-        'DNA1 (0.5)': ['pACBB_4-5', 'pACBB_4-5', 'pACBB_4-5', 'pACBB_4-5', 'pACBB_4-5'],
-        'DNA2 (0.75)': ['pACBB_vec-F1', 'pACBB_vec-F2', 'pACBB_T7-F1', 'pACBB_T7-F2', 'pET28_MCS-F1'],
-        'DNA3 (0.75)': ['pACBB_vec-R1', 'pACBB_vec-R2', 'pACBB_T7-R1', 'pACBB_T7-R2', 'pET28_MCS-R1'],
-        'Enzyme (12.5)': ['KODone', 'KODone', 'KODone', 'KODone', 'KODone'],
-        'DW (25)': ['DW', 'DW', 'DW', 'DW', 'DW']
-        }),
-        num_rows='dynamic',
-        use_container_width=True
-    )
+            st.toggle("Long Form", key=f'{plate_type}_plate_{i}_toggle')
+            if not st.session_state[f'{plate_type}_plate_{i}_toggle']:
+                st.session_state[f'{plate_type}_plate_{i}_df'] = st.data_editor(data=st.session_state[f'{plate_type}_plate_{i}_df'],
+                                                                use_container_width=True,
+                                                                key=f'{plate_type}_plate_{i}_wide')
+            else:
+                st.session_state[f'{plate_type}_plate_{i}_df'] = plate_transformation(df = st.data_editor(plate_transformation(st.session_state[f'{plate_type}_plate_{i}_df'], data_form='long'),
+                                                                                            use_container_width=True, key=f'{plate_type}_plate_{i}_long'),
+                                                                        data_form = 'wide')
+            st.file_uploader("Upload", key=f'{plate_type}_plate_{i}_uploader', disabled=True)
 
-with st.expander("Assembly", expanded=True):
-    st.data_editor(
-        pd.DataFrame({
-            'Well': ['A9', 'B9', 'C9', 'D9', 'E9'],
-            'Name': ['vec1', 'vec2', 'vec3', 'vec4', 'vec5'],
-            'DNA1': ['S1', 'S1', 'S2', 'S1', 'S3'],
-            'DNA2': ['S2', 'S3', 'S3', 'S2', None],
-            'DNA3': [None, None, None, 'S3', None],
-            'Enzyme (10)': ['NEBuilder HiFi Master Mix'] * 5,
-            'DW (20)': ['DW'] * 5
-        }),
-        num_rows='dynamic',
-        use_container_width=True
-    )
+plate_type = 'Reaction'
+with st.expander("Reaction - PCR", expanded=True):
+    reaction_type = 'PCR'
+    number_of_plate = st.number_input(f"Number of {plate_type}_plate", min_value=1, step=1, value=1, key=f'num_of_{plate_type}_plate',
+                                      disabled=True, help='This Method can only one reaction Plate')
+    plates = st.tabs([f'{plate_type}_Plate_{i+1}' for i in range(number_of_plate)])
 
-with st.expander("Transformation (Plotting)", expanded=True):
-    if not st.toggle('Long Form'):
-        st.session_state['tf_df'] = st.data_editor(st.session_state['tf_df'],
-                                                   use_container_width=True)
-    else:
-        st.session_state['tf_df'] = plate_transformation(df = st.data_editor(plate_transformation(st.session_state['tf_df'], type='long'),
-                                                                                            use_container_width=True),
-                                                                        type = 'wide')
+    for i in range(len(plates)):
+        plate_initialization(i, plate_type=plate_type, table_type='empty')
+        plate_initialization(i, plate_type=reaction_type, table_type='PCR')
+        with plates[i]:
+            st.text_input("Name", value=st.session_state[f'{plate_type}_plate_{i}_name'], key=f'{plate_type}_plate_{i}_name')
+            st.selectbox("Labware", options=st.session_state['labwares'], key=f'{plate_type}_plate_{i}_labware')
+            col1, col2 = st.columns([10, 2])
+            with col1:
+                st.session_state[f'{reaction_type}_plate_{i}_df'] = st.data_editor(
+                    data = st.session_state[f'{reaction_type}_plate_{i}_df'],
+                    num_rows='dynamic',
+                    use_container_width=True,
+                    key=f'{plate_type}_{reaction_type}_plate_{i}_wide'
+                )
+            with col2:
+                with st.form(key=f'{plate_type}_{reaction_type}_plate_{i}_addcolumn'):
+                    st.selectbox('Add Column', ['DNA', 'Enzyme'], label_visibility='collapsed', key=f'{plate_type}_{reaction_type}_plate_{i}_selecttype')
+                    if st.form_submit_button('Add Column'):
+                        # Add new column in this plate
+                        pass
+
+with st.expander("Reaction - Assembly", expanded=True):
+    reaction_type = 'Assembly'
+    plates = st.tabs([f'{plate_type}_Plate_{i+1}' for i in range(number_of_plate)])
+    
+    for i in range(len(plates)):
+        plate_initialization(i, plate_type=reaction_type, table_type='Assembly')
+        with plates[i]:
+            col1, col2 = st.columns([10, 2])
+            with col1:
+                st.session_state[f'{reaction_type}_plate_{i}_df'] = st.data_editor(
+                    data = st.session_state[f'{reaction_type}_plate_{i}_df'],
+                    num_rows='dynamic',
+                    use_container_width=True,
+                    key=f'{plate_type}_{reaction_type}_plate_{i}_wide'
+                )
+            with col2:
+                with st.form(key=f'{plate_type}_{reaction_type}_plate_{i}_addcolumn'):
+                    st.selectbox('Add Column', ['DNA', 'Enzyme'], label_visibility='collapsed', key=f'{plate_type}_{reaction_type}_plate_{i}_selecttype')
+                    if st.form_submit_button('Add Column'):
+                        # Add new column in this plate
+                        pass
+
+## Transformation
+with st.expander("Transformation", expanded=True):
+    plate_type = 'TF'
+    number_of_plate = st.number_input(f"Number of {plate_type}_plate", min_value=1, step=1, value=1, key=f'num_of_{plate_type}_plate')
+    plates = st.tabs([f'{plate_type}_Plate_{i+1}' for i in range(number_of_plate)])
+
+    for i in range(len(plates)):
+        plate_initialization(i, plate_type=plate_type, table_type='empty')
+        with plates[i]:
+            st.text_input("Name", value=st.session_state[f'{plate_type}_plate_{i}_name'], key=f'{plate_type}_plate_{i}_name')
+            st.selectbox("Labware", options=st.session_state['labwares'], key=f'{plate_type}_plate_{i}_labware')
+            
+            st.toggle("Long Form", key=f'{plate_type}_plate_{i}_toggle')
+            if not st.session_state[f'{plate_type}_plate_{i}_toggle']:
+                st.session_state[f'{plate_type}_plate_{i}_df'] = st.data_editor(data=st.session_state[f'{plate_type}_plate_{i}_df'],
+                                                                use_container_width=True,
+                                                                key=f'{plate_type}_plate_{i}_wide')
+            else:
+                st.session_state[f'{plate_type}_plate_{i}_df'] = plate_transformation(df = st.data_editor(plate_transformation(st.session_state[f'{plate_type}_plate_{i}_df'], data_form='long'),
+                                                                                            use_container_width=True, key=f'{plate_type}_plate_{i}_long'),
+                                                                        data_form = 'wide')            
